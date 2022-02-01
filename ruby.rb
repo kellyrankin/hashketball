88f693cb069b5c51502b335edeab5fe8d3556962
Kelly Rankin <kelly.rankin@kin.com>
2022-02-01 10:47:12 -0800

    Initial commit

---
+++ ruby.rb
@@ -0,0 +1,9 @@
+require 'pry'
+
+teachers = [
+    {name: "Antonio", campus: "Austin"}, 
+    {name:"Joe", campus: "Houston"}, 
+    {name: "Raul", campus: "Houston"}, 
+    {name:"Joelle", campus:"Austin"}
+]
+
